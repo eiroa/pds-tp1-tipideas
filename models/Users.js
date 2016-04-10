e0e9d795db81cfcc5d403d4184a65f43844c3380
eiroa <eiroa@gmail.com>
2016-04-10 17:16:48 -0300

    Initial commit, starting from tp0Mean fully finished

---
+++ models/Users.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase:true,unique:true},
+  hash: String,
+  salt: Date
+});
+
+
+UserSchema.methods.generateJWT = function() {
+
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET');
+};
+
+UserSchema.methods.setPassword = function(password){
+  try{
+	console.log("trying to encode password: "+password);
+
+	this.hash =crypto.createHash('sha256').update(password).digest('base64');
+
+   }catch(e){
+	console.log(e);
+	throw e;
+   }
+  
+};
+
+UserSchema.methods.validPassword = function(password) {
+
+  return this.hash === crypto.createHash('sha256').update(password).digest('base64');
+};
+
+
+mongoose.model('User', UserSchema);

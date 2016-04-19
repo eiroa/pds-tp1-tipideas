15f0976b1ce2c4b7e8bc25ab73b6490f49b88a8f
Mauro Adolfo Eiroa <eiroam@gmail.com>
2016-04-19 13:23:31 -0300

    Update Logger.js

--- models/Logger.js
+++ models/Logger.js
@@ -10,6 +10,35 @@ var LoggerSchema = new mongoose.Schema({
 LoggerSchema.methods.createdPost = function(cb,author, date){
 	this.author = author;
 	this.date = date;
+	this.description= author + " proposed a new idea ";
+	this.save(cb);
+};
+
+LoggerSchema.methods.deletedIdea = function(cb,author, date){
+	this.author = author;
+	this.date = date;
+	this.description= author + " deleted an idea ";
+	this.save(cb);
+};
+
+LoggerSchema.methods.acceptedEnrollment = function(cb,author, date){
+	this.author = author;
+	this.date = date;
+	this.description= author + " approved an enrollment to an idea ";
+	this.save(cb);
+};
+
+
+LoggerSchema.methods.rejectedIdea = function(cb,author, date){
+	this.author = author;
+	this.date = date;
+	this.description= author + " rejected an idea ";
+	this.save(cb);
+};
+
+LoggerSchema.methods.createdPost = function(cb,author, date){
+	this.author = author;
+	this.date = date;
 	this.description= author + " created a post ";
 	this.save(cb);
 };

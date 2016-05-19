b589cd6df5d07abb8e2366ded159e4859cec4a8b
eiroa <eiroa@gmail.com>
2016-05-19 05:13:09 -0300

    Controller separation from angular app core script

---
+++ public/javascripts/controllers/IdeaController.js
@@ -0,0 +1,34 @@
+var app = angular.module('tips');
+
+app.controller('ideasCtrl', [
+	'$scope',
+	'ideas',
+	'idea',
+	'auth',
+	function($scope,ideas,idea,auth){
+
+		$scope.idea = idea;
+		$scope.isLoggedIn = auth.isLoggedIn;
+		
+		
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+
+			ideas.addComment(idea._id, {
+				body: $scope.body,
+				author: 'user',
+				date: Date.now()
+			}).success(function(comment) {
+				$scope.idea.comments.push(comment);
+			});
+
+			$scope.body = '';
+		};
+		
+		$scope.addVote = function(comment){
+			ideas.like('/ideas/' + idea._id + '/comments/'+comment._id+'/upvote',comment)
+		};
+		$scope.downVote = function(comment){
+			ideas.dislike('/ideas/' + idea._id + '/comments/'+comment._id+'/downvote',comment)
+		};
+	}]);
